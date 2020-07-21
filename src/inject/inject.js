f42d3767ae26b669131e0c5085d355abf799a92f
Libao Jin <jinlibao@outlook.com>
2020-07-21 02:37:54 -0600

    Release v0.3.0
    * Optimize strategy to locating cells of "payorId" and "Add Transaction"
    * Add the functionality to find all the payorId's and validate the user-provided payorId
    * Add the feature to import multiple 1099-B forms by adding payorId eventListener
    * Update "getTotalProceeds" accordingly due to multiple 1099-B forms
    * Increase the readability for HTML by using multi-line string
    * Make UI more native by conforming the stylesheet provided Glacier Tax Prep
    * Reorganize the instruction and add a disclaimer
    * Fix whitespaces and fix the format of certain lines

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,137 +1,200 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+
+    var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+
+        var payorIds = getPayorIds();
+        var payorId = payorIds.length == 1 ? payorIds[0] : "";
 
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = "button {display: inline-block;position: relative;width: 120px;height: 32px;line-height: 32px;border-radius: 2px;font-size: 1.5em;background-color: #2196F3;color: #ffffff;transition: box-shadow 0.2s cubic-bezier(0.4, 0, 0.2, 1);transition-delay: 0.2s;}";
+        css.innerHTML = ".myInput {margin-left: 20px;}";
         document.body.appendChild(css);
-        $("#main").prepend("<div id='helper' style='padding: 10px; border-style: solid;'></div>");
-        $("#helper").prepend("<div style='display:none;color:white;font-size:1.2em;padding:10px;background-color:red;' id='errorMessage'></div>");
-        $("#helper").prepend("<button style='display:none;' id='process'>Start</button>");
-        $("#helper").prepend("<input id='fileInput' type='file' id='input' accept='text/*,.csv'>");
-        $("#helper").prepend("<h6>If you get Consolidated Form 1099 PDF from Robinhood, <a href='https://jiahaoshan.github.io/Robinhood-1099-B-Transactions-Export-Tool' target='_blank'>you may export it to CSV.</a></h6>");
-        $("#helper").prepend("<h5 id='fileInputInfo'>Please select csv file. For more information, <a target='_blank' href='https://chrome.google.com/webstore/detail/glacier-tax-1099-b-stock/mdnkfhpikjbgedlenpdnkhnfhdaohfcm?hl=en-US&gl=US'>visit here</a>. </br></br> Format Example (keep headers the same and replace green parts with your own transactions): </br></br>name,acquired,sold,proceeds,cost</br><span style='color:#4db6ac'>ALPHABET INC CLASS A COMMON STOCK,10/19/2016,11/03/2016,779.07,824.52</br>TESLA MOTORS INC,12/02/2016,12/06/2016,1468.19,1460.64</span></h5>");
-        $("#helper").prepend("<h4>Glacier Tax 1099-B Helper (Chrome Extension)</h4>");
+        var importerHTML = `
+            <div id='helper'>
+                <h2>Glacier Tax Prep Form 1099-B Stock Transactions Importer</h2>
+                <fieldset>
+                    <legend>Instruction</legend>
+                    <ol>
+                        <li>
+                            Have all the 1099-B added in the previous page.
+                            If you get Consolidated Form 1099 PDF from Robinhood, <a href='https://jiahaoshan.github.io/Robinhood-1099-B-Transactions-Export-Tool' target='_blank'>you may export it to .csv file</a>.
+                        </li>
+                        <li>Enter a valid Payor ID (EIN).</li>
+                        <li>
+                            Choose the corresponding .csv file. Make sure your .csv file follow the format strictly as below: </br>
+                            name,acquired,sold,proceeds,cost</br>
+                            ALPHABET INC CLASS A COMMON STOCK,10/19/2016,11/03/2016,779.07,824.52</br>
+                            TESLA MOTORS INC,12/02/2016,12/06/2016,1468.19,1460.64
+                        </li>
+                        <li>If both "Payor ID (EIN)" and the selected .csv file are valid, then the "Import" button will appear. Click "Import" to start importing transactions.</li>
+                    </ol>
+                    <ul>
+                        <li>
+                            Disclaimer: This Chrome extension is NOT an official tool from Glacier Tax Prep.
+                        </li>
+                        <li>
+                            Please submit an <a href='https://github.com/jinlibao/Glacier-Tax-Prep-Form-1099-B-Stock-Transactions-Importer/issues/new/choose'>issue</a> on <a href='https://github.com/jinlibao/Glacier-Tax-Prep-Form-1099-B-Stock-Transactions-Importer'>GitHub</a> for bug reports and feature requests.
+                            For more information, please visit <a target='_blank' href='https://chrome.google.com/webstore/detail/glacier-tax-1099-b-stock/mdnkfhpikjbgedlenpdnkhnfhdaohfcm?hl=en-US&gl=US'>here</a>.
+                        </li>
+                    </ul>
+                </fieldset>
+                <fieldset>
+                    <legend>Enter Payor ID (EIN) and Choose Corresponding .csv File</legend>
+                    <span class='myInput'>Payor ID (EIN): <input type='text' placeholder='Please Enter Payor ID (EIN)' id='payorId' value=${payorId}></span>
+                    <span class='myInput'><input id='fileInput' type='file' id='input' accept='text/*,.csv'></span>
+                    <span class='myInput'><button style='display:none;' id='process'>Start</button></span>
+                    <span class='myInput' style='display:none;color:white;font-size:1.2em;background-color:red;' id='errorMessage'></span>
+                </fieldset>
+            </div>
+        `;
+        $("#main").prepend(importerHTML);
 
         var fileInput = document.getElementById('fileInput');
+        payorId = document.getElementById("payorId");
+
+        payorId.addEventListener('focusout', function(e) {
+            $("#errorMessage").hide();
+            if (!validatePayorId()) {
+                resetStatus();
+                $("#process").hide();
+                $("#errorMessage").css("background-color","orange");
+                $("#errorMessage").show().html("WARNING: Invalid Payor ID.");
+                return;
+            }
+            $("#errorMessage").hide();
+            if (fileInput.value == "") {
+             resetStatus();
+             return;
+            }
+            importFile(fileInput.files[0]);
+        })
 
         fileInput.addEventListener('change', function(e) {
-            if (fileInput.value == "") return;
-            var file = fileInput.files[0];
-            var textType = /.*\.csv/;
-
-            if (file.name.match(textType)) {
-                var reader = new FileReader();
-
-                reader.onload = function(e) {
-                    var lines = processData(reader.result);
-                    if (!lines) {
-                        fileInput.value = "";
-                        return;
-                    }
-                    chrome.storage.local.set({ "transactions": lines, "status" : 'ready', file: fileInput.value}, function(){
-                        console.log("ready!");
-                        hideErrorMessage();
-                        $("#process").show();
-                        $("#process").html("Start");
-                    });
-                }
-
-                reader.readAsText(file);    
-            } else {
+            $("#errorMessage").hide();
+            if (fileInput.value == "") {
+                resetStatus();
                 $("#process").hide();
-                $("#errorMessage").css("background-color","red");
-                showErrorMessage("ERROR: only csv with header 'name,acquired,sold,proceeds,cost' is supported!");
+                $("#errorMessage").css("background-color","orange");
+                $("#errorMessage").show().html("WARNING: Invalid Input File.");
+                return;
             }
+            $("#errorMessage").hide();
+            if (!validatePayorId()) {
+                resetStatus();
+                $("#process").hide();
+                $("#errorMessage").css("background-color","orange");
+                $("#errorMessage").show().html("WARNING: Invalid Payor ID.");
+                return;
+            }
+            importFile(fileInput.files[0]);
         });
 
-        chrome.storage.local.get(["transactions", "status"], function(items){
+        chrome.storage.local.get(['transactions', 'status', 'payorId', 'index', 'file'], function(items) {
             var transactions = items.transactions;
-            // $("#Name").closest("tr").index()
+            var payorId = items.payorId;
+            var index = items.index;
+            var file = items.file;
+
             if (items.status == 'working') {
-                $("#fileInput").hide();
-                $("#fileInputInfo").hide();
-                $("#process").show().html("Working...").prop("disabled",true);
+                $("#process").show().html("Importing...").prop("disabled",true);
                 var filling = $("#Name").length ? true : false;
                 if (filling) {
-                    var index = $("#Name").closest("tr").index() - 1;
                     var transaction = transactions[index];
+                    chrome.storage.local.set({'index': index + 1}, function() {
+                        console.log("Increment index by 1");
+                    });
+                    if (index + 1 >= transactions.length) {
+                        chrome.storage.local.set({'status': 'done'}), function() {
+                            location.reload(false);
+                        }
+                    }
                     $("#Name").val(transaction[0]);
                     $("#PurchasedDateString").val(transaction[1]);
                     $("#SoldDateString").val(transaction[2]);
                     $("#SalesPrice").val(transaction[3]);
                     $("#PurchasePrice").val(transaction[4]);
                     document.forms[0].submit(); return false;
-                }
-                else {
-                    var trs = $("tbody").find("tr");
-                    for (var i = 1; i < trs.length - 1; i++) {
-                        var tr = trs[i];
-                        if (i > transactions.length) {
-                            // to many records
-                            $(tr).find("td:nth-child(8) a:nth-child(2)")[0].click();
-                            return;
-                        }
-                        var transaction = transactions[i-1];
-                        var name = $(tr).find("td:nth-child(1)").text().trim();
-                        var acquired = new Date($(tr).find("td:nth-child(2)").text().trim());
-                        var sold = new Date($(tr).find("td:nth-child(3)").text().trim());
-                        var proceed = parseFloat($(tr).find("td:nth-child(4)").text().trim());
-                        var cost = parseFloat($(tr).find("td:nth-child(5)").text().trim());
-                        var rname = transaction[0];
-                        var racquired = new Date(transaction[1]);
-                        var rsold = new Date(transaction[2]);
-                        var rproceed = parseFloat(transaction[3]);
-                        var rcost = parseFloat(transaction[4]);
-                        if(name != rname || 
-                            acquired.getTime() != racquired.getTime() ||
-                            sold.getTime() != rsold.getTime() ||
-                            proceed != rproceed ||
-                            cost != rcost
-                            ) {
-                            $(tr).find("td:nth-child(8) a:nth-child(1)")[0].click();
+                } else {
+                    var trs = $('tbody').find('tr');
+                    for (var i = 0; i < trs.length; ++i) {
+                        var isTarget = $(trs[i]).find("td")[0].innerText == payorId;
+                        if (isTarget && index < transactions.length) {
+                            $(trs[i]).find("td:nth-child(4) a:nth-child(1)")[0].click()
                             return;
                         }
                     }
-                    if (trs.length != (transactions.length + 2)) {
-                        $("tbody tr:nth-child(1) td:nth-child(4) a:nth-child(1)")[0].click();
-                        return;
-                    }
-                    else {
-                        chrome.storage.local.set({"status" : 'done'}, function(){
-                            location.reload(false);
-                        });
-                    }
                 }
-            }
-            else if (items.status == 'done') {
-                $("#errorMessage").css("background-color","green").html("Finished!");
+            } else if (items.status == 'done') {
+                $("#errorMessage").css("background-color","green").html("Imported " + file + " successfully!");
                 $("#errorMessage").show();
             }
         });
 
         $("#process").click(function() {
-            chrome.storage.local.get(["status"], function(items){
+            chrome.storage.local.get(['status'], function(items) {
                 if (items.status == 'ready') {
-                    chrome.storage.local.set({"status" : 'working'}, function(){
+                    chrome.storage.local.set({'status' : 'working'}, function() {
                         location.reload(false);
                     });
                 }
-            });     
+            });
         });
-	}
-	}, 10);
+    }
+    }, 10);
 });
 
+function resetStatus() {
+    chrome.storage.local.get(['transactions', 'status', 'payorId', 'index', 'file'], function(items) {
+        var transactions = items.transactions;
+        var status = items.status;
+        var payorId = items.payorId;
+        var index = items.index;
+        var file = items.file;
+
+        if (status == 'done' && index > 0) {
+            console.log(index);
+            status = 'not ready';
+            chrome.storage.local.set({ 'transactions': transactions, 'status' : status, 'payorId': payorId, 'index': 0, 'file': file}, function() {
+                console.log('Reset status to "not ready".');
+            });
+        }
+    });
+}
+
+function importFile(file) {
+    var textType = /.*\.csv/;
+    if (file.name.match(textType)) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var lines = processData(reader.result);
+            if (!lines) {
+                fileInput.value = "";
+                return;
+            }
+            chrome.storage.local.set({ 'transactions': lines, 'status' : 'ready', 'payorId': payorId.value, 'index': 0, 'file': file.name}, function() {
+                console.log("ready!");
+                hideErrorMessage();
+                $("#process").show();
+                $("#process").html("Import");
+            });
+        }
+
+        reader.readAsText(file);
+    } else {
+        $("#process").hide();
+        showErrorMessage("ERROR: only csv with header 'name,acquired,sold,proceeds,cost' is supported!");
+    }
+}
 
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     if (headers.length != 5) {
         showErrorMessage('ERROR: Invalid number of headers');
-    	return false;
+        return false;
     }
     if (headers[0] != 'name' || headers[1] != 'acquired' || headers[2] != 'sold' || headers[3] != 'proceeds' || headers['4'] != 'cost') {
         showErrorMessage("ERROR: only csv with header 'name,acquired,sold,proceeds,cost' is supported!");
@@ -143,12 +206,12 @@ function processData(allText) {
         if (data.length == headers.length) {
             var tarr = [];
             for (var j=0; j<headers.length; j++) {
-            	if (headers[j] == 'acquired') {
-            		var acquiredData = data[j].split(' ');
-            		if (acquiredData && acquiredData.length > 1) {
-            			data[j] = acquiredData[acquiredData.length - 1];
-            		}
-            	}
+                if (headers[j] == 'acquired') {
+                    var acquiredData = data[j].split(' ');
+                    if (acquiredData && acquiredData.length > 1) {
+                        data[j] = acquiredData[acquiredData.length - 1];
+                    }
+                }
                 tarr.push(data[j]);
             }
             lines.push(tarr);
@@ -158,11 +221,12 @@ function processData(allText) {
     return validateInputFile(lines) ? lines : false;
 }
 
-function hideErrorMessage()  {
+function hideErrorMessage()    {
     $("#errorMessage").hide();
 }
 
 function showErrorMessage(message) {
+    $("#errorMessage").css("background-color","red");
     $("#errorMessage").show().html(message);
 }
 
@@ -174,8 +238,32 @@ function validateDate(dateString) {
     return dateString.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/) ? true : false;
 }
 
+function getPayorIds() {
+    var payorIds = [];
+    var trs = $('tbody').find('tr');
+    for (var i = 0; i < trs.length; ++i) {
+        if (trs[i].childElementCount == 4) {
+            if ($(trs[i]).find("td:nth-child(2)")[0].innerText == "Total Proceeds (Box 2) 1099-B") {
+                payorIds.push($(trs[i]).find("td:nth-child(1)")[0].innerText);
+            }
+        }
+    }
+    return payorIds;
+}
+
+function validatePayorId() {
+    return getPayorIds().includes(payorId.value);
+}
+
 function getTotalProceeds() {
-    return $("tbody tr:nth-child(1) td:nth-child(3)").text().trim();   
+    if (!validatePayorId()) return -1;
+    var trs = $('tbody').find('tr');
+    for (var i = 0; i < trs.length; ++i) {
+        var isTarget = $(trs[i]).find("td")[0].innerText == payorId.value;
+        if (isTarget) {
+            return $(trs[i]).find("td:nth-child(3)").text().trim();
+        }
+    }
 }
 
 function getOrdinalIndicator(number) {
@@ -199,14 +287,14 @@ function validateInputFile(lines) {
         if (!lines[i][2] || !validateDate(lines[i][2])) {
             showErrorMessage(getErrorMessage(i, "invalid sold date"));
             return false;
-        }        
+        }
         if (!lines[i][3] || isNaN(lines[i][3]) || parseFloat(lines[i][3]) < 0) {
             showErrorMessage(getErrorMessage(i, "invalid proceeds number"));
-            return false;            
+            return false;
         }
         if (!lines[i][4] || isNaN(lines[i][4]) || parseFloat(lines[i][4]) < 0) {
             showErrorMessage(getErrorMessage(i, "invalid cost number"));
-            return false;            
+            return false;
         }
         var acquiredDate = new Date(lines[i][1]);
         var soldDate = new Date(lines[i][2]);
@@ -218,7 +306,9 @@ function validateInputFile(lines) {
     }
     var inputTotal = inputTotalProceeds.toFixed(2);
     var totalProceeds = parseFloat(getTotalProceeds()).toFixed(2);
-    if (inputTotal != totalProceeds) {
+    if (totalProceeds == -1.00) {
+        return false;
+    } else if (inputTotal != totalProceeds) {
         alert("Total proceeds in the input file (" + inputTotal + ") doesn't match the total proceeds in the 1099 (" + totalProceeds + "). You may proceed, but it may cause errors.");
         return true;
     }
